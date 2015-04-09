321f29a404f700f4c8656bb25ab2ec5daec1a599
RuSSlan4ik <ruslan11>
2015-04-09 14:52:59 +0300

    Создан form.php

---
+++ views/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+<h1>Новость</h1>
+<article>
+  <h3>
+    <?php echo $article['title']; ?>
+  </h3>
+
+  <div><?php echo $article['text_news']; ?></div>
+</article>
+</br>
+<div><a href="/php2.local/">Назад</a></div>
+</body>
+</html>
