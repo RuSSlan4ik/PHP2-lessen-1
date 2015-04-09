9a2f6d9fce22ee51b9563f419d4ef2f400466d36
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 16:39:30 +0300

    правка адресов

--- views/form.php
+++ views/form.php
@@ -7,13 +7,13 @@
 <body>
 <h1>Форма добавления новости</h1>
 
-<form action="/php2.local/form.php" method="post">
+<form action="../form.php" method="post">
   <p><input type="text" name="title" placeholder="Заголовок новости" size="25"/></p>
 
   <p><textarea name="text_news" cols="100" rows="8" placeholder="Текст новости"></textarea></p>
 
   <p><button role="button" type="submit">Отправить</button></p>
 </form>
-<a href="/php2.local/">Назад</a>
+<a href="../">Назад</a>
 </body>
 </html>
