d477ffb1d0101f02cb7586205de0205b6060de80
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 17:09:19 +0300

    Добавлен автор

--- views/form.php
+++ views/form.php
@@ -8,6 +8,8 @@
 <h1>Форма добавления новости</h1>
 
 <form action="../form.php" method="post">
+  <p><input type="text" name="author_name" placeholder="Ваше имя" size="25"/></p>
+
   <p><input type="text" name="title" placeholder="Заголовок новости" size="25"/></p>
 
   <p><textarea name="text_news" cols="100" rows="8" placeholder="Текст новости"></textarea></p>
