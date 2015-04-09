31ce57efc85aa2dfec10d5382bff22c78345ac09
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 17:25:29 +0300

    добавлен автор

--- views/article.php
+++ views/article.php
@@ -9,7 +9,7 @@
   <article>
     <h3>
       <?php echo $article['title']; ?>
-    </h3>
+    </h3><p>Автор новости:<?php echo $article['author_name']; ?></p>
 
     <div><?php echo $article['text_news']; ?></div>
   </article>
