89c297165a767069b82d2e5b7422ce17943d7b76
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 16:48:49 +0300

    foreach

--- views/article.php
+++ views/article.php
@@ -6,13 +6,15 @@
 </head>
 <body>
 <h1>Новость</h1>
-<article>
-  <h3>
-    <?php echo $article['title']; ?>
-  </h3>
+<?php foreach ($news as $article) : ?>
+  <article>
+    <h3>
+      <?php echo $article['title']; ?>
+    </h3>
 
-  <div><?php echo $article['text_news']; ?></div>
-</article>
+    <div><?php echo $article['text_news']; ?></div>
+  </article>
+<?php endforeach; ?>
 <br/>
 <div><a href="../">Назад</a></div>
 </body>
