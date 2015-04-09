cf536d3912a8961526ad8406a1f0ad6bb428e655
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 17:46:38 +0300

    время

--- models/one.php
+++ models/one.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__ . '/../functions/db.php';
-$dat = date("Y-m-d");
 function addNews($author_name, $title, $text_news, $dat)
 {
   dbConnect();
+  $dat = date("Y-m-d");
   $author_name = mysql_real_escape_string($author_name);
   $title = mysql_real_escape_string($title);
   $text_news = mysql_real_escape_string($text_news);
