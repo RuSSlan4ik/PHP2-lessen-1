571fde4d8a6a9efd399d369a40ffa670b6586beb
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 17:14:00 +0300

    Добавлен автор

--- form.php
+++ form.php
@@ -2,5 +2,5 @@
 include __DIR__ . '/views/form.php';
 require __DIR__ . '/models/one.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  addNews($_POST['title'], $_POST['text_news']);
+  addNews($_POST['author_name '], $_POST['title'], $_POST['text_news']);
 }
