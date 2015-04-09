bec2975a6c000d4f38c0830eb1223c63ac14d2d8
RuSSlan4ik <ruslan11>
2015-04-09 15:00:07 +0300

    правка1 form.php

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <?php
 include __DIR__ . '/views/form.php';
-require __DIR__ . '/models/form.php';
+require __DIR__ . '/models/one.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   addNews($_POST['title'], $_POST['text']);
 }
