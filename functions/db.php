da26a017ec6a41f624b669aa263db134ae20681a
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 16:02:43 +0300

    редактирование2

--- functions/db.php
+++ functions/db.php
@@ -9,7 +9,7 @@ function dbFindAllByQuery($sql)
   dbConnect();
   $res = mysql_query($sql);
   $ret = array();
-  while (($row = mysql_fetch_array($res))!== false) {
+  while(($row = mysql_fetch_array($res)) !== false) {
     $ret[] = $row;
   }
   return $ret;
