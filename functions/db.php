d094c665e39e6ce3196b0a90f2e60844aa0e0de1
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 16:17:23 +0300

    доступ

--- functions/db.php
+++ functions/db.php
@@ -1,12 +1,9 @@
 <?php
-function dbConnect()
-{
-  mysql_connect('localhost', 'root', '')or die("MySQL недоступен! ".mysql_error());
-  mysql_select_db('Lessen')or die("Нет соединения с базой! ".mysql_error());
-}
+mysql_connect('localhost', 'root', '');
+mysql_select_db('Lessen');
+mysql_query("SET NAMES utf8");
 function dbFindAllByQuery($sql)
 {
-  dbConnect();
   $res = mysql_query($sql);
   $ret = array();
   while(($row = mysql_fetch_array($res)) !== false) {
