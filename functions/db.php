d512870bb2dab0ff429c7726e3fb16c9b44bdf7e
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 16:30:34 +0300

    Вход

--- functions/db.php
+++ functions/db.php
@@ -1,9 +1,13 @@
 <?php
-mysql_connect('localhost', 'root', '');
-mysql_select_db('Lessen');
-mysql_query("SET NAMES utf8");
+function dbConnect()
+{
+  mysql_connect("localhost", "root", "")or die("Error");
+  mysql_select_db("site1")or die("Error");
+  mysql_query("SET NAMES utf8");
+}
 function dbFindAllByQuery($sql)
 {
+  dbConnect();
   $res = mysql_query($sql);
   $ret = array();
   while(($row = mysql_fetch_array($res)) !== false) {
