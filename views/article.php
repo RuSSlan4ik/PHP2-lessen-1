efa1d11184dc55efaa80ee659fc9e723e071b660
RuSSlan4ik <ru.hus@mail.ru>
2015-04-09 16:53:12 +0300

    убрал foreach

--- views/article.php
+++ views/article.php
@@ -6,7 +6,6 @@
 </head>
 <body>
 <h1>Новость</h1>
-<?php foreach ($news as $article) : ?>
   <article>
     <h3>
       <?php echo $article['title']; ?>
@@ -14,7 +13,6 @@
 
     <div><?php echo $article['text_news']; ?></div>
   </article>
-<?php endforeach; ?>
 <br/>
 <div><a href="../">Назад</a></div>
 </body>
