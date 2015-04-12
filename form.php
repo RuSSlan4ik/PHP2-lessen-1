718d7f3478f67ab6359644cb9c7923c787d36f63
RuSSlan4ik <ru.hus@mail.ru>
2015-04-12 22:26:39 +0300

    Delete form.php

--- form.php
+++
@@ -1,6 +0,0 @@
-<?php
-include __DIR__ . '/views/form.php';
-require __DIR__ . '/models/one.php';
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  addNews($_POST['author_name'], $_POST['title'], $_POST['text_news'], $_POST['data']);
-}
