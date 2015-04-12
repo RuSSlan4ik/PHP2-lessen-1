10f35946944448701ea131006629ef3de60f213d
RuSSlan4ik <ru.hus@mail.ru>
2015-04-12 22:35:03 +0300

    база

--- lesson1/models/news.php
+++ lesson1/models/news.php
@@ -2,13 +2,13 @@
 require __DIR__ .'db.php';
 function findAllNews()
 {
-  $sql = 'SELECT * FROM NEWS ORDER BY data DESC';
+  $sql = 'SELECT * FROM news ORDER BY data DESC';
   $ret = dbFindAllByQuery($sql);
   return $ret;
 }
 function findOneById($id)
 {
-  $sql = 'SELECT * FROM NEWS WHERE id=' . $id;
+  $sql = 'SELECT * FROM news WHERE id=' . $id;
   $ret = dbFindOneByQuery($sql);
   return $ret;
 }
